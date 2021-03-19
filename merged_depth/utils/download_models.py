0721588ec1287d62de2afa9e134f307d42439c39
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-03-19 18:22:48 -0500

    Added downloader for pretrained models

---
+++ merged_depth/utils/download_models.py
@@ -0,0 +1,62 @@
+'''
+Guide for usage:
+In your terminal, run the command:
+python download_gdrive.py GoogleFileID /path/for/this/file/to/download/file.type
+Credited to 
+https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
+author: https://stackoverflow.com/users/1475331/user115202
+'''
+
+import requests
+from tqdm import tqdm
+import os
+import zipfile
+
+def download_file_from_google_drive(id, destination):
+  def get_confirm_token(response):
+    for key, value in response.cookies.items():
+      if key.startswith('download_warning'):
+        return value
+
+    return None
+
+  def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+      with tqdm(unit='B', unit_scale=True, unit_divisor=1024) as bar:
+        for chunk in response.iter_content(CHUNK_SIZE):
+          if chunk:  # filter out keep-alive new chunks
+            f.write(chunk)
+            bar.update(CHUNK_SIZE)
+
+  URL = "https://docs.google.com/uc?export=download"
+
+  session = requests.Session()
+
+  response = session.get(URL, params = { 'id' : id }, stream = True)
+  token = get_confirm_token(response)
+
+  if token:
+    params = { 'id' : id, 'confirm' : token }
+    response = session.get(URL, params = params, stream = True)
+
+  save_response_content(response, destination)
+
+def download_models(gdrive_fileid, output_path):
+  output_path = os.path.join(output_directory, output_filename)
+  if not os.path.exists(output_path):
+    download_file_from_google_drive(gdrive_fileid, output_path)
+
+def extract_models(file):
+  dirname = os.path.dirname(file)
+  with zipfile.ZipFile(file, 'r') as zip_ref:
+    zip_ref.extractall(dirname)
+
+def main():
+  pretrained_models = os.path.join("pretrained", "pretrained.zip")
+  download_models(gdrive_fileid="1Pw7PVsPJISzPurkAvYByNY6egDqdPFMt", output_path=pretrained_models)
+  extract_models(pretrained_models)
+
+if __name__ == '__main__':
+  main()
