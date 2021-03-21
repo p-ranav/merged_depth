e047203b52136527296163d0ce0150f8105c70c4
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-03-20 23:58:25 -0500

    Added convenience script to predict depth for a single image

---
+++ merged_depth/infer_single.py
@@ -0,0 +1,31 @@
+import argparse
+import cv2
+import os
+import numpy as np
+from .infer import InferenceEngine
+
+def main(path):
+
+  def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+  engine = InferenceEngine()
+
+  dirname = os.path.dirname(path)
+  base = os.path.basename(path)
+  filename_minus_ext, ext = os.path.splitext(base)
+
+  print("Predicting depth for image", path)
+
+  image, depth, colorized_depth = engine.predict_depth(path)
+  display = np.vstack([image, colorized_depth])
+
+  cv2.imwrite(os.path.join(dirname, filename_minus_ext + "_depth" + ext), display)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Run the depth predictor on input image')
+  parser.add_argument('path', metavar='path', type=str,
+                      help='path to input image')
+  args = parser.parse_args()
+  main(args.path)
