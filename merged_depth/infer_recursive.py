bfd0764baef39a2ea614d8bcdee42a4f604f5128
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-03-27 19:49:48 -0500

    Updated infer recursive to work with no_grad and exception handling on a large .PNG of .JPG dataset

--- merged_depth/infer_recursive.py
+++ merged_depth/infer_recursive.py
@@ -4,30 +4,32 @@ import os
 import numpy as np
 from .infer import InferenceEngine
 from tqdm import tqdm
+import torch
 
 def main(path):
 
   inputs = []
   for root, dirs, files in os.walk(path, topdown=False):
     for file in files:
-      if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
+      if (not file.startswith(".")) and (file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg")):
         inputs.append(os.path.join(root, file))
 
-  engine = InferenceEngine()
-  for path in tqdm(inputs):
-    try:
-      dirname = os.path.dirname(path)
-      base = os.path.basename(path)
-      filename_minus_ext, ext = os.path.splitext(base)
+  with torch.no_grad():
+    engine = InferenceEngine()
+    for path in tqdm(inputs):
+      try:
+        dirname = os.path.dirname(path)
+        base = os.path.basename(path)
+        filename_minus_ext, ext = os.path.splitext(base)
 
-      image, depth, colorized_depth = engine.predict_depth(path)
+        image, depth, colorized_depth = engine.predict_depth(path)
 
-      # Save numpy array of color+depth values
-      save_path = os.path.join(dirname, filename_minus_ext + ".npz")
-      np.savez_compressed(save_path, color=image, depth=depth)
-    except Exception as e:
-      print("Exception thrown while processing image", path)
-      print(e)
+        # Save numpy array of color+depth values
+        save_path = os.path.join(dirname, filename_minus_ext + ".npz")
+        np.savez_compressed(save_path, color=image, depth=depth)
+      except Exception as e:
+        print("Exception thrown while processing image", path)
+        print(e)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Run the depth predictor recursively on all .jpeg/.png files in directory (ideal for datasets)')
